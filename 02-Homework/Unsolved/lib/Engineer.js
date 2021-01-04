675cb5cb25f461fb75ed28ac9b3ff4eefe7e3582
Biaka Z <biaka@biakaworks.com>
2021-01-04 02:05:33 -0600

    updated engineer.js

--- 02-Homework/Unsolved/lib/Engineer.js
+++ 02-Homework/Unsolved/lib/Engineer.js
@@ -4,23 +4,14 @@ const Employee = require("./Employee");
 
 // The Letter Class is responsible for displaying either an underscore or the underlying character for each letter in the word
 class Engineer extends Employee{
-    constructor(myName, myId, myEmail, mygithub) {
-        super(myName,myId, myEmail);
-        //this will create an instance of employee
 
+    constructor(myName, myId, myEmail, github) {
+        super(myName,myId, myEmail, "Engineer");
+        this.github = github;
     }
-  
-    // Returns either an underscore or the underlying character depending on `this.visible`
-    //    because we call this function toString, when we call `this.letters.join` in
-    //    Word.js, JavaScript automatically uses the value we return here
-    getName() {
-      return this.name;
+    getGithub() {
+      return this.github;
     }
-
-    getRole() {
-        return 'Employee';
-    }
-  
   }
   
   module.exports = Engineer;
